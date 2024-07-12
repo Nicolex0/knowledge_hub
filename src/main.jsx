bb84a9bb857395b85875f9180572d93b4bf9b1de
John-Alexander Kamau <xander.kamau7@gmail.com>
2024-07-12 08:55:55 +0300

    resolve merge conflicts

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,21 @@
-import { RouterProvider, createBrowserRouter} from 'react-router-dom';
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import store from './store/store.js'
-import Home from './pages/Home.jsx'
-import { AuthLayout, Login } from './components/index.js'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import { AuthLayout, Login } from './components/index.js';
+import './index.css';
+import Home from './pages/Home.jsx';
+import store from './store/store.js';
 
-import { MantineProvider } from '@mantine/core'; 
 import '@mantine/carousel/styles.css';
+import { MantineProvider } from '@mantine/core';
 
 import AddPost from "./pages/AddPost";
-import Signup from './pages/Signup'
+import AllPosts from "./pages/AllPosts";
 import EditPost from "./pages/EditPost";
 import Post from "./pages/Post";
-import AllPosts from "./pages/AllPosts";
+import Signup from './pages/Signup';
 
 
 const router = createBrowserRouter([
@@ -141,8 +141,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       </MantineProvider>
     </Provider>
   </React.StrictMode>,
-<<<<<<< HEAD
 )
-=======
-)
->>>>>>> 45aaec14e8adcc2d3e1253a24346fad011f0fa9d
